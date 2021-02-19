1ea3a2b7fccadb66e120b8c65539a604e19551dd
ishowman <16897283+ishowman@users.noreply.github.com>
2021-02-19 10:25:51 +0800

    fix: url

--- src/components/WxApp/Index.vue
+++ src/components/WxApp/Index.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="wx-app">
-    <div v-if="openFail" class="mask">
-      <img src="./imgs/arrow.png" alt="" class="arrow" />
-      <div class="message">
-        <p>1. 点击右上角选择</p>
-        <p>2. 选择浏览器打开</p>
-      </div>
-      <!-- <img src="./imgs/logo.png" alt="" class="logo" /> -->
-    </div>
     <!-- 尝试用定位 + z-index 控制样式 -->
-    <wx-open-launch-app id="btn" :appid="appid266" :extinfo="extinfo" class="btn-open-app" 
+    <wx-open-launch-app id="btn" :appid="appid266" :extinfo="`?extend=${extinfo}`" class="btn-open-app wx-btn" 
       v-show="isWexin"
       style="z-index: 9;"
       @click="$emit('onClick', $event)"
@@ -27,7 +19,7 @@
         <div class="btn">&emsp;</div>
       </script>
     </wx-open-launch-app>
-    <div @click="openApp($event)" class="btn-open-app" >
+    <div @click="openApp($event)" class="btn-open-app grid-center" >
       <slot />
     </div>
   </div>
@@ -38,12 +30,20 @@
 import Vue from 'vue'
 import sha1 from 'js-sha1'
 import { wechatmp } from '@/idl/bgo/component/wechatmp'
-
+function isAndroid () {
+  const ua = navigator.userAgent.toLowerCase().toString()
+  console.log('isAndroid',(ua.includes("android") || ua.includes("adr")))
+  return (ua.includes("android") || ua.includes("adr"))
+}
 export default Vue.extend({
   props: {
     extinfo: {
       type: String,
-      required: true
+      // default: location.search
+    },
+    failCb: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -59,33 +59,42 @@ export default Vue.extend({
     }
   },
   mounted () {
+    console.log(101010)
     this.wxInited()
   },
   methods: {
     async wxInited() {
+      const wx = window['jWeixin'] || require('weixin-js-sdk') // 修复运行时提示 .config of undefind 的报错
+
       const timestamp = new Date().getTime()
       const result = await wechatmp.WechatmpServer.getJsTicket(new wechatmp.GetJsTicketReq())
       console.log('tic', result)
-      if (result) {
+      if(!result) return
+      // if (result) {
         this.ticket = result.jsTicket
-      }
+      // }
       window.localStorage.setItem('jsapi_ticket', JSON.stringify(this.ticket))
       const nonceStr = 'boyaa.com'
-      const unSign = `jsapi_ticket=${this.ticket}&noncestr=${nonceStr}&timestamp=${timestamp}&url=${location.href}`
+      const isAnd= isAndroid()
+      const url = isAnd? location.href : location.origin+location.pathname
+      console.log('url', url )
+
+      const unSign = `jsapi_ticket=${this.ticket}&noncestr=${nonceStr}&timestamp=${timestamp}&url=${url}`
       const signature = sha1(unSign)
       // eslint-disable-next-line
       wx.config({
-        // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
+        debug: window.DEBUG, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
         appId: this.wxPublicAppid, // 必填，公众号的唯一标识
         timestamp, // 必填，生成签名的时间戳
         nonceStr, // 必填，生成签名的随机串
         signature, // 必填，签名
-        jsApiList: ['onMenuShareAppMessage'], // 必填，需要使用的JS接口列表
+        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'], // 必填，需要使用的JS接口列表
         openTagList: this.openTagList // 可选，需要使用的开放标签列表，例如['wx-open-launch-app']
       })
 
       // eslint-disable-next-line
       wx.ready(() => {
+        console.log('wx-ready')
 
         const btn = document.getElementById('btn')
         console.log('ready?', this.openTagList, btn)
@@ -100,22 +109,32 @@ export default Vue.extend({
           btn.addEventListener('error', (e: any) => {
             console.log('error', e.detail)
             this.openFail = true
+            this.$emit('open-failed', e)
+            if(!this.failCb) this.noApp()
           })
         }
+
+        this.$emit('wx-ready')
       })
     },
     openApp (event: EventTarget) {
       this.$emit('onClick', event)
-      console.log('yxdt266://' + location.search);
-      window.location.href = 'yxdt266://' + location.search
+      const base = ({
+        'test': 'yxdt266test',
+        'pre':'yxdt266pre',
+        'prod': 'yxdt266'
+      } as any)[process.env.VUE_APP_CLIENT_ENV]
+      console.log(`${base}://?extend=` + this.extinfo);
+      window.location.href = `${base}://?extend=` + this.extinfo
       this.noApp()
     },
     noApp () {
       this.timer = setTimeout(() => {
         console.log('noApp')
         if(this.timer) clearTimeout(this.timer)
-        !(document as any).webkitHidden && !document.hidden && (location.href = 'https://www.266.com')
-      }, 2500)
+        this.$emit('open-failed')
+        if(!this.failCb) !(document as any).webkitHidden && !document.hidden && (location.href = 'https://www.266.com')
+      }, 2000)
     },
 
   }
@@ -127,6 +146,8 @@ export default Vue.extend({
 <style scoped>
 .wx-app {
   position: relative;
+  width: 100%;
+  height: 100%;
 }
 .mask {
   position: fixed;
